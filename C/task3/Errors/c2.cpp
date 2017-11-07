a0c2a7b1e7248c08f1f3cdd4788c60d661681cfe
01110001 <33186765+01110001@users.noreply.github.com>
2017-11-08 00:56:25 +0300

    Add files via upload

---
+++ C/task3/Errors/c2.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+
+int b=1,k,x,s=0;
+
+    printf("Enter the one that you wanna be turned into binary style!11! : ");
+	scanf("%f",&x);
+	
+   
+while(x>0)
+{   
+    x%2==k;
+    s=s+(k*b);
+	b=b*10;
+	x=x/2;
+	
+	
+	
+	
+	
+}
+
+
+printf ("Here binary comes! : %f" ,&s);
+
+
+
+
+
+
+
+
+
+			return 0;
+}
+
+
